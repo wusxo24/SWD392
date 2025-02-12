9a9ab2500895eebd0c8ce0e15091265fe17d8dfa
Leminh04 <leminh04012004@gmail.com>
2025-02-12 13:06:50 +0700

    change

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const connectDB = require("./config/db");
 const authRoute = require("./routes/auth.route");
 const Product = require('./models/product.model.js');
 const productRoute = require("./routes/product.route.js");
+const applyMiddleware = require("./middleware");
 const app = express()
 
 app.use(express.json());
@@ -12,13 +14,15 @@ app.get('/', (req, res) => {
     res.send("Hello World !");
 });
 
+// Apply middleware
+applyMiddleware(app);
+
 // routes
 app.use("/api/products", productRoute);
-
 app.use("/api/auth", authRoute);
 
-
-mongoose.connect("mongodb+srv://leminh:leminh04012004@backenddb.ocbpn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackEndDB")
+// Connect to database and start server
+connectDB()
     .then(() => {
         console.log("Connect to database!");
         app.listen(3000, () => {
