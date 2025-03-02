47fbbbb46ef43f87d7adba22d855899eaa02bd8a
WuKai12 <wusxocompany@gmail.com>
2025-03-02 21:12:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// Middleware
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', process.env.FRONT_END_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-
 
 // Apply middleware
 applyMiddleware(app);
