49db776e9eda0dc69835d225f23157610fb2c7ec
TranTrongAn1 <Cacchuxai@gmail.com>
2025-03-05 01:00:50 +0700

    add add children for member but only UI, fix navbar close fuction

--- src/page/profile.jsx
+++ src/page/profile.jsx
@@ -1,21 +1,45 @@
-import { Button, TextField } from "@mui/material";
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  MenuItem,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import axios from "../utils/axiosInstance";
 
 const Profile = () => {
-  const userId = localStorage.getItem("userId") || sessionStorage.getItem("userId");
+  const userId =
+    localStorage.getItem("userId") || sessionStorage.getItem("userId");
   const [member, setMember] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
   const [isEditing, setIsEditing] = useState(false);
   const [formData, setFormData] = useState({});
-  
+  const [imageUrl, setImageUrl] = useState(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ3aF4GtX_wOJ75UD0DAwM2yjUmzRIOO6ssA&s"
+  );
+
+  // Add Child state variables
+  const [showChildrenPopup, setShowChildrenPopup] = useState(false);
+  const [childFname, setChildFname] = useState("");
+  const [childLname, setChildLname] = useState("");
+  const [childBirthdate, setChildBirthdate] = useState("");
+  const [childGender, setChildGender] = useState("Male");
+  const [childPicture, setChildPicture] = useState("");
+  const [childBloodType, setChildBloodType] = useState("");
+  const [childAllergy, setChildAllergy] = useState("");
+  const [childNotes, setChildNotes] = useState("");
+
   useEffect(() => {
     const fetchMember = async () => {
       try {
         const response = await axios.get(`api/members/${userId}`);
         setMember(response.data);
         setFormData(response.data);
+        setImageUrl(response.data.avatar); // Set image URL from member data
       } catch (err) {
         setError("Failed to fetch member.");
       } finally {
@@ -30,6 +54,10 @@ const Profile = () => {
 
   const handleEditClick = () => {
     setIsEditing(true);
+  };
+
+  const handleImageChange = (event) => {
+    setImageUrl(event.target.value);
   };
 
   const handleInputChange = (e) => {
@@ -50,10 +78,14 @@ const Profile = () => {
       alert("Invalid email format.");
       return;
     }
-    
+
     try {
-      await axios.put(`api/members/${userId}`, formData);
-      setMember(formData);
+      // Assuming your backend updates the avatar URL as well
+      await axios.put(`api/members/${userId}`, {
+        ...formData,
+        avatar: imageUrl,
+      });
+      setMember({ ...formData, avatar: imageUrl });
       setIsEditing(false);
       alert("Profile updated successfully!");
     } catch (err) {
@@ -61,9 +93,57 @@ const Profile = () => {
     }
   };
 
-  if (loading) return <p className="text-center text-lg font-semibold">Loading...</p>;
-  if (error) return <p className="text-center text-red-500 text-lg font-semibold">{error}</p>;
-  if (!member) return <p className="text-center text-gray-500">No member found.</p>;
+  const handleAddChildClick = () => {
+    setShowChildrenPopup(true);
+  };
+
+  const handleCloseChildPopup = () => {
+    setShowChildrenPopup(false);
+    setChildFname("");
+    setChildLname("");
+    setChildBirthdate("");
+    setChildGender("Male");
+    setChildPicture("");
+    setChildBloodType("");
+    setChildAllergy("");
+    setChildNotes("");
+  };
+
+  const handleSaveChild = async () => {
+    try {
+      await axios.post(`api/children/${userId}`, {
+        fname: childFname,
+        lname: childLname,
+        birthdate: childBirthdate,
+        gender: childGender,
+        picture: childPicture,
+        blood_type: childBloodType,
+        allergy: childAllergy,
+        notes: childNotes,
+      });
+      handleCloseChildPopup();
+      alert("Child added successfully!");
+
+      // Refresh member data after adding a child (optional)
+      const response = await axios.get(`api/members/${userId}`);
+      setMember(response.data);
+      setFormData(response.data);
+    } catch (error) {
+      console.error("Failed to add child:", error);
+      alert("Failed to add child.");
+    }
+  };
+
+  if (loading)
+    return <p className="text-center text-lg font-semibold">Loading...</p>;
+  if (error)
+    return (
+      <p className="text-center text-red-500 text-lg font-semibold">
+        {error}
+      </p>
+    );
+  if (!member)
+    return <p className="text-center text-gray-500">No member found.</p>;
 
   return (
     <div className="p-10 shadow-lg bg-[#f9f9f9]">
@@ -71,13 +151,18 @@ const Profile = () => {
       <div className="p-6 rounded-lg bg-white">
         <div className="flex gap-4 items-center mb-4">
           <img
-            src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ3aF4GtX_wOJ75UD0DAwM2yjUmzRIOO6ssA&s"}
+            src={imageUrl}
             alt="Avatar"
             className="rounded-full w-30 h-30 object-cover bg-[#f9f9f9] shadow-md"
           />
+
           <div>
-            <h3 className="text-xl font-semibold">{member.fullname || "No Name"}</h3>
-            <p className="text-gray-600">{member.email || "No Email Provided"}</p>
+            <h3 className="text-xl font-semibold">
+              {member.fullname || "No Name"}
+            </h3>
+            <p className="text-gray-600">
+              {member.email || "No Email Provided"}
+            </p>
           </div>
           <div style={{ marginLeft: "auto" }}>
             {isEditing ? (
@@ -89,10 +174,17 @@ const Profile = () => {
                 Edit
               </Button>
             )}
+            <Button
+              variant="contained"
+              onClick={handleAddChildClick}
+              style={{ marginLeft: "10px" }}
+            >
+              Add Child
+            </Button>
           </div>
         </div>
 
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4 mt-10">
           <div>
             <TextField
               label="Full Name"
@@ -101,6 +193,7 @@ const Profile = () => {
               onChange={handleInputChange}
               fullWidth
               disabled={!isEditing}
+              style={{ marginBottom: "30px", backgroundColor: "#f9f9f9" }}
             />
             <TextField
               label="Phone"
@@ -110,6 +203,7 @@ const Profile = () => {
               fullWidth
               disabled={!isEditing}
               className="mt-4"
+              style={{ backgroundColor: "#f9f9f9" }}
             />
           </div>
           <div>
@@ -120,6 +214,7 @@ const Profile = () => {
               onChange={handleInputChange}
               fullWidth
               disabled={!isEditing}
+              style={{ marginBottom: "30px", backgroundColor: "#f9f9f9" }}
             />
             <TextField
               label="Address"
@@ -129,10 +224,90 @@ const Profile = () => {
               fullWidth
               disabled={!isEditing}
               className="mt-4"
+              style={{ backgroundColor: "#f9f9f9" }}
             />
           </div>
         </div>
       </div>
+
+      {/* Add Child Popup */}
+      <Dialog open={showChildrenPopup} onClose={handleCloseChildPopup}>
+        <DialogTitle>Add Child</DialogTitle>
+        <DialogContent>
+          {/* Input fields for child information */}
+          <TextField
+            label="Child's First Name"
+            value={childFname}
+            onChange={(e) => setChildFname(e.target.value)}
+            fullWidth
+            style={{ marginBottom: "30px", backgroundColor: "#f9f9f9" }}
+          />
+          <TextField
+            label="Child's Last Name"
+            value={childLname}
+            onChange={(e) => setChildLname(e.target.value)}
+            fullWidth
+            style={{ marginBottom: "30px", backgroundColor: "#f9f9f9" }}
+          />
+          <TextField
+            label="Child's Birthdate"
+            type="date"
+            value={childBirthdate}
+            onChange={(e) => setChildBirthdate(e.target.value)}
+            fullWidth
+            InputLabelProps={{
+              shrink: true,
+            }}
+            style={{ marginBottom: "30px", backgroundColor: "#f9f9f9" }}
+          />
+          <TextField
+            label="Child's Gender"
+            select
+            value={childGender}
+            onChange={(e) => setChildGender(e.target.value)}
+            fullWidth
+            style={{ marginBottom: "20px" }}
+          >
+            <MenuItem value="Male">Male</MenuItem>
+            <MenuItem value="Female">Female</MenuItem>
+            <MenuItem value="Other">Other</MenuItem>
+          </TextField>
+          <TextField
+            label="Child's Picture URL"
+            value={childPicture}
+            onChange={(e) => setChildPicture(e.target.value)}
+            fullWidth
+            style={{ marginBottom: "30px", backgroundColor: "#f9f9f9" }}
+          />
+          <TextField
+            label="Child's Blood Type"
+            value={childBloodType}
+            onChange={(e) => setChildBloodType(e.target.value)}
+            fullWidth
+            style={{ marginBottom: "30px", backgroundColor: "#f9f9f9" }}
+          />
+          <TextField
+            label="Child's Allergy"
+            value={childAllergy}
+            onChange={(e) => setChildAllergy(e.target.value)}
+            fullWidth
+            style={{ marginBottom: "30px", backgroundColor: "#f9f9f9" }}
+          />
+          <TextField
+            label="Child's Notes"
+            value={childNotes}
+            onChange={(e) => setChildNotes(e.target.value)}
+            fullWidth
+            style={{ backgroundColor: "#f9f9f9" }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseChildPopup}>Cancel</Button>
+          <Button onClick={handleSaveChild} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
