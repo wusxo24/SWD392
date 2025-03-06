b234ffd35360b4562939e4273ce14fd9ac1e342c
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-03-06 14:11:19 +0700

    "fixed bugs which I didnt know existed"

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
 
 const applyMiddleware = (app) => {
   app.use(express.json());
-  app.use(cookieParser());
   app.use(express.urlencoded({ extended: false }));
 };
 
--- routes/MedicalRequestRoutes.js
+++ routes/MedicalRequestRoutes.js
@@ -5,8 +5,8 @@ const medicalRequestController = require("../controllers/MedicalRequestControlle
 
 router.post("/:recordId", authMiddleware, medicalRequestController.createMedicalRequest);
 router.get("/:recordId", authMiddleware, authorize(["Manager"]), medicalRequestController.getMedicalRequestByRecordId);
-router.put("/accept/:medicalRequestId", authMiddleware, authorize(["Manager"]), medicalRequestController.AcceptMedicalRequest);
-router.put("/reject/:medicalRequestId", authMiddleware, authorize(["Manager"]), medicalRequestController.RejectMedicalRequest);
+router.put("/accept/:medicalRequestId", authMiddleware, authorize(["Manager"]), medicalRequestController.acceptMedicalRequest);
+router.put("/reject/:medicalRequestId", authMiddleware, authorize(["Manager"]), medicalRequestController.rejectMedicalRequest);
 router.get("/doctor/:doctorId", authMiddleware, authorize(["Doctor"]), medicalRequestController.getMedicalRequestByDoctorId);
 router.put("/doctor/:medicalRequestId", authMiddleware, authorize(["Doctor"]), medicalRequestController.doctorStartWorkingOnMedicalRequest);
 
