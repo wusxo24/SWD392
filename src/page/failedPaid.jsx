2fb598da96dfa58191c0e25aba9893e9b57fe5ba
TranTrongAn1 <Cacchuxai@gmail.com>
2025-03-10 13:43:44 +0700

    change ../utils/axiosInstance to @/utils/axiosInstance

--- src/page/failedPaid.jsx
+++ src/page/failedPaid.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import axios from "../utils/axiosInstance";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import axios from "@/utils/axiosInstance";
 export const FaildedPaid = () => {
     const { id } = useParams(); // Get order ID from URL
     const [order, setOrder] = useState(null);
