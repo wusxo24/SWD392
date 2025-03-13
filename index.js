540d56461d0082d49e2affa5199a78a79d4a92d5
hukoFpt <lynnie.nguyen.bns@gmail.com>
2025-03-13 10:52:04 +0700

    Update CORS configuration to allow all origins

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const app = express();
 
 // Enable CORS
 app.use(cors({
-  origin: process.env.FRONT_END_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
