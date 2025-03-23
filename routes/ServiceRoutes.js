d52c065da925a94631b2e26119921115f17ffd91
WuKai12 <wusxocompany@gmail.com>
2025-03-23 10:58:38 +0700

    Update ServiceRoutes.js

--- routes/ServiceRoutes.js
+++ routes/ServiceRoutes.js
@@ -4,8 +4,8 @@ const { authMiddleware, authorize } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/", getAllServices); // Public route - anyone can view services
-router.post("/", authMiddleware, authorize(["Admin"]), createService); // Only Admins can create
+router.post("/", authMiddleware, authorize(["Manager"]), createService); // Only Admins can create
 router.put("/:id", authMiddleware, updateService); // Only Admins can update
-router.delete("/:id", authMiddleware, authorize(["Admin"]), deleteService); // Only Admins can delete
+router.delete("/:id", authMiddleware, authorize(["Manager"]), deleteService); // Only Admins can delete
 
 module.exports = router;
